4c6ab0c4abfbe11ef422be20e325d6c036bb0379
James Camac <james.camac@gmail.com>
2017-10-06 16:14:01 +1100

    Updating code

--- R/data_processing.R
+++ R/data_processing.R
@@ -6,12 +6,12 @@ read_csv(file, col_types=cols_only(
   Species = "c",
   Tribe = "c",
   Tribe_ID = "i",
-  Tillers_prefire = "i",
+  Tillers_cm2 = "d",
   Tillers_postfire = "i",
   Type = col_factor(c("C3","C4")),
   LDMC = "d")) %>%
   mutate(Treatment_ID = as.numeric(Treatment),
          Type_ID = as.numeric(Type)) %>%
-    select(Treatment, Treatment_ID, Tribe, Tribe_ID, Species, Tillers_prefire, Tillers_postfire, Type, Type_ID, LDMC)
+    select(Treatment, Treatment_ID, Tribe, Tribe_ID, Species, Tillers_prefire = Tillers_cm2, Tillers_postfire, Type, Type_ID, LDMC)
 }
 
--- R/model.R
+++ R/model.R
@@ -21,16 +21,20 @@ run_stan_model <- function(data, sim_pretillers=NULL, sim_ldmc = NULL) {
     n_obs = nrow(data),
     n_tribe = max(data$Tribe_ID),
     tribe = data$Tribe_ID,
+    tribe_name = data$Tribe,
     n_spp = length(unique(data$Species)),
     spp = as.numeric(factor(data$Species)),
-    treatment = data$Treatment_ID,
+    spp_name = data$Species,
+    treatment = data$Treatment_ID-1,
     tillers_prefire = (data$Tillers_prefire - mn_pretillers)/(2*sd_pretillers),
     ldmc = (data$LDMC - mn_ldmc)/(2*sd_ldmc),
-    c4 = data$Type_ID,
+    c4 = data$Type_ID-1,
     sim_nobs_pretillers = length(sim_pretillers), # number of pre tiller simulations 
     sim_nobs_ldmc = length(sim_ldmc), # number of ldmc simulations
-    sim_pretillers = (sim_pretillers - mn_pretillers) / (2 * sd_pretillers), # CS TPRE values for predictions
-    sim_ldmc = (sim_ldmc - mn_ldmc)/ (2 * sd_ldmc), # CS LDMC values for predictions
+    sim_pretillers = sim_pretillers,
+    sim_ldmc = sim_ldmc,
+    cs_sim_pretillers = (sim_pretillers - mn_pretillers) / (2 * sd_pretillers), # CS TPRE values for predictions
+    cs_sim_ldmc = (sim_ldmc - mn_ldmc)/ (2 * sd_ldmc), # CS LDMC values for predictions
     y = data$Tillers_postfire)
 
 model ="
@@ -47,11 +51,11 @@ data{
   int<lower=0> y[n_obs];
   int<lower=0> sim_nobs_pretillers;
   int<lower=0> sim_nobs_ldmc;
-  real sim_pretillers[sim_nobs_pretillers]; 
-  real sim_ldmc[sim_nobs_ldmc]; 
+  real cs_sim_pretillers[sim_nobs_pretillers]; 
+  real cs_sim_ldmc[sim_nobs_ldmc]; 
 }
 parameters{ 
-  real a_tribe[n_tribe];
+  real a_raw_tribe[n_tribe];
   real a_tribe_mu;
   real<lower=0> a_tribe_sd;
   real a_spp[n_spp];
@@ -60,7 +64,7 @@ parameters{
   real a_ldmc;
   real a_c4;
   real a_tpre;
-  real b_tribe[n_tribe];
+  real b_raw_tribe[n_tribe];
   real b_tribe_mu;
   real<lower=0> b_tribe_sd;
   real b_spp[n_spp];
@@ -71,19 +75,27 @@ parameters{
   real b_tpre;
   real<lower=0, upper=100> phi;
 }
-transformed parameters { 
+transformed parameters {
+  real a_tribe[n_tribe];
+  real b_tribe[n_tribe];
   real count[n_obs];
   real p_death[n_obs];
-  
+
+  for (t in 1:n_tribe) {
+  // same as normal(a_tribe_mu, a_tribe_sd)
+  a_tribe[t] = a_raw_tribe[t] * a_tribe_sd + a_tribe_mu;
+  // noncentered param: same as normal(b_tribe_mu, b_tribe_sd)
+  b_tribe[t] = b_raw_tribe[t] * b_tribe_sd + b_tribe_mu; 
+  }
   for (i in 1:n_obs) {
-    count[i] = exp(b_tribe[tribe[i]] + b_spp[spp[i]] + b_trt * treatment[i] + b_ldmc * ldmc[i] + b_c4 * c4[i] + b_tpre * tillers_prefire[i]);
-    p_death[i] = inv_logit(a_tribe[tribe[i]] + a_spp[spp[i]] + a_trt * treatment[i] + a_ldmc * ldmc[i] + a_c4 * c4[i] + a_tpre * tillers_prefire[i]);
+    count[i] = exp(b_tribe[tribe[i]] + b_spp[spp[i]] + b_trt * treatment[i] + b_c4 * c4[i] + b_ldmc * ldmc[i] + b_tpre * tillers_prefire[i]);
+    p_death[i] = inv_logit(a_tribe[tribe[i]] + a_spp[spp[i]] + a_trt * treatment[i] + a_c4 * c4[i] + a_ldmc * ldmc[i] + a_tpre * tillers_prefire[i]);
   }
 }
 model { 
   a_tribe_mu ~ cauchy(0,5);
   a_tribe_sd ~ cauchy(0,5);
-  a_tribe ~ normal(a_tribe_mu, a_tribe_sd);
+  a_raw_tribe ~ normal(0,1);
   a_spp ~ normal(0, a_spp_sd);
   a_spp_sd ~ cauchy(0,5);
   a_trt ~ cauchy(0,5);
@@ -92,7 +104,7 @@ model {
   a_tpre ~ cauchy(0,5);
   b_tribe_mu ~ cauchy(0,5);
   b_tribe_sd ~ cauchy(0,5);
-  b_tribe ~ normal(b_tribe_mu, b_tribe_sd);
+  b_raw_tribe ~ normal(0,1);
   b_spp ~ normal(0, b_spp_sd);
   b_spp_sd ~ cauchy(0,5);
   b_trt ~ normal(0,5);
@@ -100,14 +112,14 @@ model {
   b_c4 ~ normal(0,5);
   b_tpre ~ normal(0,5);
   
-  # Likelihood
+  // Likelihood
   
   for (i in 1:n_obs){
-    if (y[i] == 0) { # if does not survive
+    if (y[i] == 0) { // if does not survive
       target +=(log_sum_exp(bernoulli_lpmf(1 | p_death[i]),
                                      bernoulli_lpmf(0 | p_death[i])
                                      + neg_binomial_2_lpmf(y[i] | count[i], phi)));
-    } else { # if survives
+    } else { // if survives
       target +=(bernoulli_lpmf(0 | p_death[i])
                          + neg_binomial_2_lpmf(y[i] | count[i], phi));
     }
@@ -142,7 +154,7 @@ generated quantities {
   real pred_count_c3_ldmc[sim_nobs_ldmc];
   real pred_count_c4_ldmc[sim_nobs_ldmc];
   
-  #Estimate log likelihood
+  // Estimate log likelihood
   for (i in 1:n_obs) {
     if (y[i]== 0) {
       log_lik[i] = log_sum_exp(bernoulli_lpmf(1 | p_death[i]),
@@ -156,7 +168,7 @@ generated quantities {
     resid[i] = (y[i] - pred[i])/sqrt(pred[i] + (phi * pred[i]^2));
   }
   
-  # Partial dependencies of TRT & C3/C4
+  // Partial dependencies of TRT & C3/C4
   pred_p_death_4day_c3 = inv_logit(a_tribe_mu);
   pred_p_death_4day_c4 = inv_logit(a_tribe_mu + a_c4);
   pred_p_death_8day_c3 = inv_logit(a_tribe_mu + a_trt);
@@ -171,20 +183,20 @@ generated quantities {
   pred_count_11day_c3 = exp(b_tribe_mu + b_trt * 2);
   pred_count_11day_c4 = exp(b_tribe_mu + b_trt * 2 + b_c4);
   
-  # Partial dependencies TPRE
+  // Partial dependencies tillers prefire
   for (j in 1:sim_nobs_pretillers) {
-    pred_p_death_c3_tpre[j] = inv_logit(a_tribe_mu + a_tpre * sim_pretillers[j]);
-    pred_p_death_c4_tpre[j] = inv_logit(a_tribe_mu + a_tpre * sim_pretillers[j] + a_c4);
-    pred_count_c3_tpre[j] = exp(b_tribe_mu + b_tpre * sim_pretillers[j]);
-    pred_count_c4_tpre[j] = exp(b_tribe_mu + b_tpre * sim_pretillers[j] + b_c4);
+    pred_p_death_c3_tpre[j] = inv_logit(a_tribe_mu + a_tpre * cs_sim_pretillers[j]);
+    pred_p_death_c4_tpre[j] = inv_logit(a_tribe_mu + a_tpre * cs_sim_pretillers[j] + a_c4);
+    pred_count_c3_tpre[j] = exp(b_tribe_mu + b_tpre * cs_sim_pretillers[j]);
+    pred_count_c4_tpre[j] = exp(b_tribe_mu + b_tpre * cs_sim_pretillers[j] + b_c4);
   }
   
-  # Partial dependencies LDMC
+  // Partial dependencies ldmc
   for (k in 1:sim_nobs_ldmc) {
-    pred_p_death_c3_ldmc[k] = inv_logit(a_tribe_mu + a_ldmc * sim_ldmc[k]);
-    pred_p_death_c4_ldmc[k] = inv_logit(a_tribe_mu + a_ldmc * sim_ldmc[k] + a_c4);
-    pred_count_c3_ldmc[k] = exp(b_tribe_mu + b_ldmc * sim_ldmc[k]);
-    pred_count_c4_ldmc[k] = exp(b_tribe_mu + b_ldmc * sim_ldmc[k] + b_c4);
+    pred_p_death_c3_ldmc[k] = inv_logit(a_tribe_mu + a_ldmc * cs_sim_ldmc[k]);
+    pred_p_death_c4_ldmc[k] = inv_logit(a_tribe_mu + a_ldmc * cs_sim_ldmc[k] + a_c4);
+    pred_count_c3_ldmc[k] = exp(b_tribe_mu + b_ldmc * cs_sim_ldmc[k]);
+    pred_count_c4_ldmc[k] = exp(b_tribe_mu + b_ldmc * cs_sim_ldmc[k] + b_c4);
     }
 }"
 
@@ -207,6 +219,7 @@ fit <- list(stan_data = stan_data,
                                 'pred_p_death_c4_ldmc', 'pred_count_c4_ldmc'),
                        cores = 3,
                        chains = 3,
-                       iter = 2000))
+                       iter = 1000,
+                       control= list(adapt_delta=0.99, max_treedepth=15)))
 return(fit)
 }
